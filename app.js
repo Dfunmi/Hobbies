5e25ea2ee26f224beb8e8a18ac69340500e59dc8
Oluwafunmilayo Owolabi <damilolaowolabi55@gmail.com>
2024-12-27 03:19:35 +0100

    bugs fixed

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
-btn.addEventListener('click', () =>{
+// Menu toggle function
+const toggleMenu = () => {
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
+}
+
+btn.addEventListener('click', toggleMenu)
+document.addEventListener('click', (e) => {
+
+    if (nav.classList.contains('flex') && 
+        !nav.contains(e.target) && 
+        !btn.contains(e.target)) {
+        toggleMenu()
+    }
 })
